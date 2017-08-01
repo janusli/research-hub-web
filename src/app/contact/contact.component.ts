2601acb33c5e824371e9804ab019c4162857231a
sina <s.ansari@auckland.ac.nz>
2017-08-01 12:05:59 +1200

    refactor and style guide compliance

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 
-const PHYSICAL_ADDRESS: string [] = ['Centre for eResearch', 'Building 302, Level 5, Room 585',
-  '23 Symonds Street', 'Auckland Central', 'Auckland 1010', 'New Zealand'];
-
-const POSTAL_ADDRESS: string [] = ['Centre for eResearch', 'The University of Auckland',
-  'Private Bag 92019', 'Auckland 1142', 'New Zealand'];
-
-const EMAIL_ADDRESS = 'eresearch-support@auckland.ac.nz';
-
-const CONTACT_PHONE = '+64 9 373 7599 ext 822231';
-
 
 @Component({
   selector: 'app-contact',
@@ -18,15 +8,20 @@ const CONTACT_PHONE = '+64 9 373 7599 ext 822231';
 })
 export class ContactComponent implements OnInit {
 
-  physical_address: string[] = PHYSICAL_ADDRESS;
-  postal_address: string [] = POSTAL_ADDRESS;
-  email_address: string = EMAIL_ADDRESS;
-  contact_phone: string = CONTACT_PHONE;
+  physicalAddress: string [] = ['Centre for eResearch', 'Building 302, Level 5, Room 585',
+    '23 Symonds Street', 'Auckland Central', 'Auckland 1010', 'New Zealand'];
 
-  constructor() {}
+  postalAddress: string [] = ['Centre for eResearch', 'The University of Auckland',
+    'Private Bag 92019', 'Auckland 1142', 'New Zealand'];
+
+  emailAddress = 'eresearch-support@auckland.ac.nz';
+
+  contactPhone = '+64 9 373 7599 ext 822231';
+
+  constructor() {
+    // TODO: set contact info variable values from a database
+  }
 
   ngOnInit() {}
-
-
 
 }
